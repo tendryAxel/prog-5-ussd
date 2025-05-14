61ec14623e37021c9a807ce5bcdc74d2efaeb781
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-05-14 09:11:15 +0300

    feat: multpile choice

---
+++ compte.py
@@ -0,0 +1,4 @@
+class Account:
+    def __init__(self, numero: str):
+        self.numero = numero
+        self.solde = 0
--- main.py
+++ main.py
@@ -3,30 +3,33 @@ from textual.widgets import Static, Input, Button, Select
 from textual.containers import Vertical
 from textual.screen import Screen
 from textual.message import Message
+from compte import Account
 
 class MainScreen(Screen):
     BINDINGS = [("q", "quit", "Quitter")]
 
     def compose(self) -> ComposeResult:
-        yield Static("Choisis un objet à louer :", id="title")
-        yield Input(placeholder="Date de début (AAAA-MM-JJ)", id="start")
-        yield Input(placeholder="Date de fin (AAAA-MM-JJ)", id="end")
+        self.account = Account("032 12 345 67")
+        self.sub_pages = {
+            "recharge": "recharge",
+            "achat": "achat",
+            "service": "service",
+            "compte": "compte"
+        }
+
+        yield Static(f"Votre Numero : {self.account.numero}", id="title")
+        yield Select(list(self.sub_pages.items()), prompt="Choice", id="selector")
         yield Button("Louer", id="submit")
         yield Static("", id="output")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         selector = self.query_one("#selector", Select)
-        start_input = self.query_one("#start", Input)
-        end_input = self.query_one("#end", Input)
         output = self.query_one("#output", Static)
         
-        selected_name = selected.name
+        selected_name = selector.value
 
         try:
-            start_date = date.fromisoformat(start_input.value.strip())
-            end_date = date.fromisoformat(end_input.value.strip())
-            loc.louer(DateRange(start_date, end_date))
-            output.update(f"[green]{selected_name} réservé ! Dates:\n{'locations'}[/green]")
+            output.update(f"[green]Redirection vers: {selected_name}[/green]")
         except Exception as e:
             output.update(f"[red]Erreur : {e}[/red]")
 
