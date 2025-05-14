ce74c5cd29d4c91c968de48e2686b7ddc27c3996
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-05-14 09:18:56 +0300

    feat: redirection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static, Input, Button, Select
+from textual.widgets import Static, Input, Button, Select, Label
 from textual.containers import Vertical
 from textual.screen import Screen
 from textual.message import Message
@@ -19,7 +19,7 @@ class MainScreen(Screen):
 
         yield Static(f"Votre Numero : {self.account.numero}", id="title")
         yield Select(list(self.sub_pages.items()), prompt="Choice", id="selector")
-        yield Button("Louer", id="submit")
+        yield Button("Submit", id="submit")
         yield Static("", id="output")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
@@ -30,8 +30,22 @@ class MainScreen(Screen):
 
         try:
             output.update(f"[green]Redirection vers: {selected_name}[/green]")
+
+            if event.button.id == "submit":
+                self.app.push_screen(selected_name)
+
         except Exception as e:
             output.update(f"[red]Erreur : {e}[/red]")
+
+
+class RechargeScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Label("Détails du paquet : requests")
+        yield Button("Retour", id="go-back")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "go-back":
+            self.app.pop_screen()
 
 
 class MainApp(App):
@@ -47,8 +61,13 @@ class MainApp(App):
     }
     """
 
+    SCREENS = {
+        "main": MainScreen,
+        "recharge": RechargeScreen
+    }
+
     def on_mount(self) -> None:
-        self.push_screen(MainScreen())
+        self.push_screen("main")
 
 
 if __name__ == "__main__":
